9508ddc6343cb89323605da30e7e2cd72753fa48
Ominitay <37453713+Ominitay@users.noreply.github.com>
2021-12-02 10:08:35 +0000

    Day 2
    aa

--- src/day02.zig
+++ src/day02.zig
@@ -11,8 +11,33 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day02.txt");
 
+const Direction = enum {
+    forward,
+    down,
+    up,
+};
+
 pub fn main() !void {
-    
+    var input = tokenize(u8, data, "\n");
+    var distance: usize = 0;
+    var depth: usize = 0;
+    var depth2: usize = 0;
+    while (input.next()) |line| {
+        if (line.len == 0) break;
+        const space_index = indexOf(u8, line, ' ').?;
+        const direction = std.meta.stringToEnum(Direction, line[0..space_index]).?;
+        const value = try parseInt(usize, line[space_index + 1 .. line.len], 10);
+        switch (direction) {
+            .up => depth -= value,
+            .down => depth += value,
+            .forward => {
+                distance += value;
+                depth2 += value * depth;
+            },
+        }
+    }
+    print("Part 1: {d}\n", .{distance * depth});
+    print("Part 2: {d}\n", .{distance * depth2});
 }
 
 // Useful stdlib functions
