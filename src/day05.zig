4525a08b29541648ade79622ab8c52a3eaf6cfc9
ominitay <37453713+ominitay@users.noreply.github.com>
2021-12-05 14:56:35 +0000

    Day 5
    aaaaa

--- src/day05.zig
+++ src/day05.zig
@@ -11,8 +11,89 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day05.txt");
 
+const Coordinates = struct {
+    x: isize,
+    y: isize,
+};
+
+const Line = struct {
+    a: Coordinates,
+    b: Coordinates,
+};
+
+const VentMap = struct {
+    map: Map(Coordinates, u8),
+    count: usize = 0,
+
+    pub fn mark(self: *VentMap, coords: Coordinates) !void {
+        const entry = try self.map.getOrPutValue(coords, 0);
+        entry.value_ptr.* += 1;
+        if (entry.value_ptr.* == 2) self.count += 1;
+    }
+};
+
 pub fn main() !void {
-    
+    var arena = std.heap.ArenaAllocator.init(gpa);
+    const allocator = &arena.allocator;
+
+    var lines = blk: {
+        var arraylist = std.ArrayList(Line).init(allocator);
+        errdefer arraylist.deinit();
+
+        var lines_iter = tokenize(u8, data, "\n");
+        while (lines_iter.next()) |line_str| {
+            var coords_iter = tokenize(u8, line_str, " ->,");
+            try arraylist.append(Line{
+                .a = .{ .x = try parseInt(isize, coords_iter.next().?, 10), .y = try parseInt(isize, coords_iter.next().?, 10) },
+                .b = .{ .x = try parseInt(isize, coords_iter.next().?, 10), .y = try parseInt(isize, coords_iter.next().?, 10) },
+            });
+        }
+
+        break :blk arraylist.toOwnedSlice();
+    };
+    defer allocator.free(lines);
+
+    var map = VentMap{ .map = Map(Coordinates, u8).init(allocator) };
+    defer map.map.deinit();
+
+    for (lines) |line| {
+        if (line.a.y == line.b.y) {
+            var x = line.a.x;
+            var end = line.b.x;
+            var i: i2 = if (x < end) 1 else -1;
+            while (x - i != end) : (x += i) {
+                try map.mark(.{ .x = x, .y = line.a.y });
+            }
+        } else if (line.a.x == line.b.x) {
+            var y = line.a.y;
+            var end = line.b.y;
+            var i: i2 = if (y < end) 1 else -1;
+            while (y - i != end) : (y += i) {
+                try map.mark(.{ .x = line.a.x, .y = y });
+            }
+        }
+    }
+
+    print("Part 1: {d}\n", .{map.count});
+
+    for (lines) |line| {
+        if (line.a.x != line.b.x and line.a.y != line.b.y) {
+            var x = line.a.x;
+            var end_x = line.b.x;
+            var x_i: i2 = if (x < end_x) 1 else -1;
+            var y = line.a.y;
+            var end_y = line.b.y;
+            var y_i: i2 = if (y < end_y) 1 else -1;
+            while (x - x_i != end_x) : ({
+                x += x_i;
+                y += y_i;
+            }) {
+                try map.mark(.{ .x = x, .y = y });
+            }
+        }
+    }
+
+    print("Part 2: {d}\n", .{map.count});
 }
 
 // Useful stdlib functions
