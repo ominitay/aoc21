d5443e4936edc5083d427abaad42a500ab9a29f6
ominitay <37453713+ominitay@users.noreply.github.com>
2021-12-07 09:37:41 +0000

    Day 7
    aaaaaaa

--- src/day07.zig
+++ src/day07.zig
@@ -12,7 +12,44 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day07.txt");
 
 pub fn main() !void {
-    
+    var arena = std.heap.ArenaAllocator.init(gpa);
+    const allocator = arena.allocator();
+
+    var crabs = blk: {
+        var iter = tokenize(u8, data, ",\n");
+        var arraylist = List(isize).init(allocator);
+        while (iter.next()) |crab| {
+            try arraylist.append(try parseInt(isize, crab, 10));
+        }
+        break :blk arraylist.toOwnedSlice();
+    };
+    sort(isize, crabs, {}, comptime asc(isize));
+
+    print("Part 1: {d}\n", .{getFuel1(crabs[crabs.len / 2], crabs)});
+    print("Part 2: {d}\n", .{getFuel2(mean(crabs), crabs)});
+}
+
+fn mean(slice: []const isize) isize {
+    var count: isize = 0;
+    for (slice) |item| count += item;
+    return @divFloor(count, @intCast(isize, slice.len));
+}
+
+fn getFuel1(position: isize, start: []const isize) isize {
+    var count: isize = 0;
+    for (start) |crab| {
+        count += std.math.absInt(position - crab) catch unreachable;
+    }
+    return count;
+}
+
+fn getFuel2(position: isize, start: []const isize) isize {
+    var count: isize = 0;
+    for (start) |crab| {
+        const n = std.math.absInt(position - crab) catch unreachable;
+        count += @divFloor(n * (n + 1), 2); // Triangular numbers
+    }
+    return count;
 }
 
 // Useful stdlib functions
